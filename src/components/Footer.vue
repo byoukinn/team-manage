7e1ac294ee619bf879e8a91a1650b9dea3383b30
Aim-Tric <473074764@qq.com>
2018-12-17 10:47:40 +0800

    add Header and Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="footer">
+        <div>
+            <p>© 2017 广东农工商职业技术学院 计算机系 ｜ 技术支持：ITeam基地 勤博工作室</p>
+            <p>地址：广东省广州市广东农工商职业技术学院增城校区</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Footer',
+    }
+</script>
+
+<style scoped>
+    .footer>div {
+        width: 50%;
+        margin: 0 auto;
+        padding: 35px;
+    }
+
+    .footer p {
+        text-align: center;
+        font-size: 12px;
+        color: #ffffff;
+    }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="header">
+            <img class="logo" src="@/assets/logo.png" alt="广东农工商职业技术学院">
+            <div class="hengfu">
+                <p>"专业社团-工作室-创业公司"育人平台的探索与实践</p>
+                <p>省级教育教学成果奖申报网站</p>
+            </div>
+
+            <div class="star">
+                <span></span>
+                <ul>
+                    <li><a href="">设为主页</a></li>
+                    <li><a href="">收藏此页</a></li>
+                </ul>
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .header {
+        position: relative;
+        display: flex;
+        padding: 30px;
+        font-weight: bold;
+    }
+
+    .logo {
+        width: 50%;
+        height: 50%;
+    }
+    
+    .hengfu {
+        margin-top: 15px;
+        display: flex;
+        flex-flow: column;
+    }
+
+
+    .hengfu>p {
+        margin: -10px;
+        font-size: 16px;
+    }
+
+    .header {
+        width: 50%;
+        margin: auto;
+    }
+
+    .star {
+        display: flex;
+        position: absolute;
+        top: 0;
+        right: 0;
+    }
+
+    .star span {
+        margin-right: -1px;
+        width: 27px;
+        height: 27px;
+        background: url('../assets/navL.png') no-repeat;
+    }
+
+    .star ul {
+        background-color: #025e3a;
+        display: flex;
+    }
+
+    .star ul li {
+        padding: 4px;
+        list-style-type: none;
+    }
+
+    .star ul li a:hover {
+        color: #f58c02;
+    }
+</style>
