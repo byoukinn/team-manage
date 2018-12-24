0bdc17b2a5f1bfa92ad5356f49465d40591c5971
byoukinn <1209418977@qq.com>
2018-12-25 05:02:50 +0800

    add Contentview

---
+++ src/components/BreadCrumb.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="row">
+    <span class="pull-left">{{prefix}}</span>
+    <ul class="pull-left breadcrumb">
+      <li v-for="(item,index) in brumblist" :key="index">
+        <router-link :to="item.path">{{item.meta.breadcrumbName}}</router-link>
+        <span class="separator" v-if="index !== brumblist.length-1">{{separator}}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    created() {
+      this.updateBreadcrumb();
+    },
+    data() {
+      return {
+        prefix: '您的位置：', // 页面标题
+        brumblist: [] // 路由集合
+      }
+    },
+    props: ['separator'],
+    methods: {
+      updateBreadcrumb() {
+        var list = [{meta: {
+            breadcrumbName: '首页'
+          },
+          path: '/',
+        }]
+        // 由于数据配置问题，在开头多了一个/content，尝试把他剔除了。
+        var r = this.$route.matched
+        var append = r.slice(1, r.length)
+        this.brumblist = list.concat(append)
+      }
+    },
+    watch: {
+      $route() {
+        this.updateBreadcrumb()
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .row {
+    padding: 0px;
+    padding-top: 0;
+    margin: 10px 15px;
+  }
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+
+  li a {
+    text-decoration: none;
+    color: #333;
+  }
+
+  li a:hover {
+    color: #20a0ff;
+    text-decoration: underline;
+  }
+
+  .separator {
+    display: inline-block;
+    padding: 0 5px;
+  }
+
+
+  .breadcrumb {
+    padding: 0 5px;
+  }
+</style>
---
+++ src/views/ContentView.vue
@@ -0,0 +1,43 @@
+<template>
+    <Panel>
+        <Article :article='content' />
+    </Panel>
+</template>
+
+<script>
+    import Panel from "@/components/Panel.vue"
+    import Article from "@/components/Article.vue"
+
+    export default {
+        name: 'ContentView',
+        data() {
+            return {
+                content: this.getArticle(),
+            }
+        },
+        components: {
+            Article,
+            Panel,
+        },
+        methods: {
+            getArticle: function() {
+                var articles = this.$store.state.MenuModule.contents
+                var name = this.$route.name
+                return articles[name]
+            }  
+        },
+        watch: {
+            // 监听路由，只要路由有变化(路径，参数等变化)都有执行下面的函数
+            $route: {
+                handler: function (to, from) {
+                    this.content = this.getArticle();
+                },
+                deep: true
+            }
+        },
+    }
+</script>
+
+<style>
+
+</style>
